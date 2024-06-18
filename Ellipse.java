4fd6391fdac7a4d42dfdfa065cfcf5aca195405e
Kauan Izidoro <159201822+KauanIzidoro@users.noreply.github.com>
2024-06-18 02:23:48 -0300

    Add files via upload

---
+++ Ellipse.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class Ellipse extends Rectangle {
+    @Override
+    public void draw(Graphics g){
+        g.setColor(super.getColor());
+        g.fillOval(super.getX(), super.getY(), super.getHeight(), super.getWidth());
+    }
+}
